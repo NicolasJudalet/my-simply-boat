b7bda434aa732e19ca3936da10f56c6d020fb479
Nico <nicolas.judalet@gmail.com>
2020-05-15 00:24:07 +0200

    :tada: Initial commit: create landing page

--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,140 @@
-import Head from 'next/head'
+/** @jsx jsx */
+import Head from 'next/head';
+import Link from 'next/link';
+import { Global, css, jsx } from '@emotion/core';
+import { darkGray, lightGray, tealBlue } from '../styles';
 
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Global
+        styles={css`
+          html,
+          body {
+            padding: 0 10px;
+            margin: 0;
+            font-family: 'Roboto', sans-serif;
+            color: ${darkGray};
+          }
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          * {
+            box-sizing: border-box;
+          }
+
+          a {
+            color: inherit;
+            text-decoration: none;
+          }
+
+          ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0 0 30px;
+            display: flex;
+            flex-direction: row;
+          }
+
+          .presentationItem {
+            flex: 1;
+            text-align: center;
+            padding: 0 40px;
+
+            h2 {
+              font-size: 1.5rem;
+            }
+          }
+
+          @media (max-width: 600px) {
+            ul {
+              width: 100%;
+              flex-direction: column;
+            }
+          }
+        `}
+      />
+      <Head>
+        <title>My Simply Boat</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&family=Roboto:wght@300;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <header
+        css={{
+          fontFamily: "'Roboto Mono', monospace",
+          fontSize: '1.5rem',
+          padding: '10px 0',
+          width: '100%',
+          borderBottom: `1px solid ${lightGray}`,
+        }}
+      >
+        <Link href="/">
+          <a>MySimplyBoat</a>
+        </Link>
+      </header>
+      <main
+        css={{
+          padding: '2rem 0',
+          flex: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <h1
+          css={{
+            color: tealBlue,
+            fontWeight: 'bold',
+            fontSize: '2.5rem',
+            textAlign: 'center',
+          }}
+        >
+          Votre bateau en propriété partagée
+          <br />
+          en toute simplicité
         </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
+        <ul css={{}}>
+          <li className="presentationItem">
+            <h2>Achetez</h2>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Trouvez le bateau qui vous convient parmi les annonces de
+              MySimplyBoat
             </p>
-          </a>
-        </div>
+          </li>
+          <li className="presentationItem">
+            <h2>Vendez</h2>
+            <p>
+              Trouvez des acheteurs pour votre bateau en postant une annonce
+            </p>
+          </li>
+          <li className="presentationItem">
+            <h2>Gérez</h2>
+            <p>
+              Gérez le planning de votre bateau en vous connectant avec vos
+              copropriétaires
+            </p>
+          </li>
+        </ul>
+
+        <img
+          src="/images/home_picture.jpg"
+          alt="home_picture"
+          css={{ width: '100%' }}
+        />
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+      <footer
+        css={{
+          width: '100%',
+          padding: '20px',
+          borderTop: `1px solid ${lightGray}`,
+          textAlign: 'center',
+        }}
+      >
+        <span>Contact : contact@mysimplyboat.fr</span>
       </footer>
 
       <style jsx>{`
@@ -68,142 +146,7 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
       `}</style>
     </div>
-  )
+  );
 }
---
+++ styles.js
@@ -0,0 +1,4 @@
+// Colors
+export const tealBlue = '#0097a7';
+export const darkGray = '#434343';
+export const lightGray = '#d2d2d2';
